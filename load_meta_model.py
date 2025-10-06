af7b6026062962f586485fec4fd19cb8ae42ea8b
AquaMouse247 <aquabanyie@gmail.com>
2025-10-06 08:37:47 -0400

    Added cropped maps and iTAML saliency files.

---
+++ load_meta_model.py
@@ -0,0 +1,195 @@
+import os
+import torch
+
+import torch.optim as optim
+import time
+import pickle
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import pdb
+import copy
+
+from resnet import *
+import random
+from Saliency.iTAML.radam import *
+
+from Saliency.iTAML import incremental_dataloader as data
+from Saliency.imports import iTAMLArgs
+from saliency_generator import load_model
+
+
+dataset = "cifar10"
+sess = 0
+
+args = iTAMLArgs
+if dataset == "cifar100":
+    args.class_per_task = 10
+    args.num_class = 100
+else:
+    args.class_per_task = 2
+    args.num_class = 10
+
+args.dataset = dataset
+args.sess = sess
+inc_dataset = data.IncrementalDataset(
+                        dataset_name=args.dataset,
+                        args = args,
+                        random_order=args.random_classes,
+                        shuffle=True,
+                        seed=1,
+                        batch_size=args.train_batch,
+                        workers=args.workers,
+                        validation_split=args.validation,
+                        increment=args.class_per_task,
+                    )
+
+def meta_test(model, memory, inc_dataset):
+    # switch to evaluate mode
+    model.eval()
+
+    meta_models = []
+    base_model = copy.deepcopy(model)
+    class_acc = {}
+    meta_task_test_list = {}
+    for task_idx in range(args.sess + 1):
+
+        memory_data, memory_target = memory
+        memory_data = np.array(memory_data, dtype="int32")
+        memory_target = np.array(memory_target, dtype="int32")
+
+        mem_idx = np.where((memory_target >= task_idx * args.class_per_task) & (
+                    memory_target < (task_idx + 1) * args.class_per_task))[0]
+        meta_memory_data = memory_data[mem_idx]
+        meta_memory_target = memory_target[mem_idx]
+        meta_model = copy.deepcopy(base_model)
+
+        meta_loader = inc_dataset.get_custom_loader_idx(meta_memory_data, mode="train", batch_size=64)
+
+        meta_optimizer = optim.Adam(meta_model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0,
+                                    amsgrad=False)
+
+        meta_model.train()
+
+        ai = args.class_per_task * task_idx
+        bi = args.class_per_task * (task_idx + 1)
+        bb = args.class_per_task * (args.sess + 1)
+        print("Training meta tasks:\t", task_idx)
+
+        # META training
+        if (args.sess != 0):
+            for ep in range(1):
+                for batch_idx, (inputs, targets) in enumerate(meta_loader):
+                    targets_one_hot = torch.FloatTensor(inputs.shape[0], (task_idx + 1) * args.class_per_task)
+                    targets_one_hot.zero_()
+                    targets_one_hot.scatter_(1, targets[:, None], 1)
+                    target_set = np.unique(targets)
+
+                    if use_cuda:
+                        inputs, targets_one_hot, targets = inputs.cuda(), targets_one_hot.cuda(), targets.cuda()
+                    inputs, targets_one_hot, targets = torch.autograd.Variable(inputs), torch.autograd.Variable(
+                        targets_one_hot), torch.autograd.Variable(targets)
+
+                    _, outputs = meta_model(inputs)
+                    class_pre_ce = outputs.clone()
+                    class_pre_ce = class_pre_ce[:, ai:bi]
+                    class_tar_ce = targets_one_hot.clone()
+
+                    loss = F.binary_cross_entropy_with_logits(class_pre_ce, class_tar_ce[:, ai:bi])
+
+                    meta_optimizer.zero_grad()
+                    loss.backward()
+                    meta_optimizer.step()
+
+        # META testing with given knowledge on task
+        meta_model.eval()
+        for cl in range(args.class_per_task):
+            class_idx = cl + args.class_per_task * task_idx
+            loader = inc_dataset.get_custom_loader_class([class_idx], mode="test", batch_size=10)
+
+            for batch_idx, (inputs, targets) in enumerate(loader):
+                targets_task = targets - args.class_per_task * task_idx
+
+                if use_cuda:
+                    inputs, targets_task = inputs.cuda(), targets_task.cuda()
+                inputs, targets_task = torch.autograd.Variable(inputs), torch.autograd.Variable(targets_task)
+
+                _, outputs = meta_model(inputs)
+
+                if use_cuda:
+                    inputs, targets = inputs.cuda(), targets_task.cuda()
+                inputs, targets_task = torch.autograd.Variable(inputs), torch.autograd.Variable(targets_task)
+
+                pred = torch.argmax(outputs[:, ai:bi], 1, keepdim=False)
+                pred = pred.view(1, -1)
+                correct = pred.eq(targets_task.view(1, -1).expand_as(pred)).view(-1)
+
+                correct_k = float(torch.sum(correct).detach().cpu().numpy())
+
+                for i, p in enumerate(pred.view(-1)):
+                    key = int(p.detach().cpu().numpy())
+                    key = key + args.class_per_task * task_idx
+                    if (correct[i] == 1):
+                        if (key in class_acc.keys()):
+                            class_acc[key] += 1
+                        else:
+                            class_acc[key] = 1
+
+        #           META testing - no knowledge on task
+        meta_model.eval()
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            if use_cuda:
+                inputs, targets = inputs.cuda(), targets.cuda()
+            inputs, targets = torch.autograd.Variable(inputs), torch.autograd.Variable(targets)
+
+            _, outputs = meta_model(inputs)
+            outputs_base, _ = model(inputs)
+            task_ids = outputs
+
+            task_ids = task_ids.detach().cpu()
+            outputs = outputs.detach().cpu()
+            outputs = outputs.detach().cpu()
+            outputs_base = outputs_base.detach().cpu()
+
+            bs = inputs.size()[0]
+            for i, t in enumerate(list(range(bs))):
+                j = batch_idx * args.test_batch + i
+                output_base_max = []
+                for si in range(args.sess + 1):
+                    sj = outputs_base[i][si * args.class_per_task:(si + 1) * args.class_per_task]
+                    sq = torch.max(sj)
+                    output_base_max.append(sq)
+
+                task_argmax = np.argsort(outputs[i][ai:bi])[-5:]
+                task_max = outputs[i][ai:bi][task_argmax]
+
+                if (j not in meta_task_test_list.keys()):
+                    meta_task_test_list[j] = [[task_argmax, task_max, output_base_max, targets[i]]]
+                else:
+                    meta_task_test_list[j].append([task_argmax, task_max, output_base_max, targets[i]])
+        del meta_model
+
+    acc_task = {}
+    for i in range(args.sess + 1):
+        acc_task[i] = 0
+        for j in range(args.class_per_task):
+            try:
+                acc_task[i] += class_acc[i * args.class_per_task + j] / args.sample_per_task_testing[i] * 100
+            except:
+                pass
+    print("\n".join([str(acc_task[k]).format(".4f") for k in acc_task.keys()]))
+    print(class_acc)
+
+    return acc_task
+
+if __name__ == '__main__':
+    memory = None
+    if sess > 0:
+        with open(f"Saliency/iTAML/{dataset}" + "/memory_" + str(args.sess - 1) + ".pickle", 'rb') as handle:
+            memory = pickle.load(handle)
+
+    _, _, _, testloader, for_memory = inc_dataset.new_task(memory)
+    memory = inc_dataset.get_memory(memory, for_memory)
+    model = load_model("iTAML", dataset, sess, args=args)
+    use_cuda = False
+    acc_task = meta_test(model, memory, inc_dataset)
---
+++ shapTest.py
@@ -0,0 +1,168 @@
+import torchvision.datasets
+import numpy as np
+import json
+import shap
+import sys
+import os
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from tqdm import tqdm
+
+# Custom Imports
+from saliency_generator import load_model
+from saliency_generator import SalGenArgs, iTAMLArgs, MnistArgs
+import saliency_dataloader as sdl
+from rps_net import generate_path
+
+
+def setup_args(algorithm, dataset) -> None:
+    if algorithm == "iTAML":
+       SalGenArgs.args = iTAMLArgs
+    else: SalGenArgs.args = MnistArgs
+
+    if dataset == "cifar100":
+        SalGenArgs.class_per_task = 10
+        SalGenArgs.num_class = 100
+    else:
+        SalGenArgs.class_per_task = 2
+        SalGenArgs.num_class = 10
+
+    SalGenArgs.algorithm = algorithm
+    SalGenArgs.dataset = dataset
+    SalGenArgs.args.dataset = SalGenArgs.dataset
+    SalGenArgs.args.num_class = SalGenArgs.num_class
+
+
+def get_train_set(dataset):
+    '''
+    Gets the train dataset for the shap explainer
+    :param dataset: Desired dataset for shap explainer
+    :return: train_set
+    '''
+    match dataset:
+        case "mnist":
+            if algorithm == "foster" or algorithm == "memo" or algorithm == "der":
+                trsf = transforms.Compose([transforms.Pad(2), transforms.ToTensor()])
+            elif algorithm == "DGR":
+                trsf = transforms.Compose([transforms.ToTensor(), transforms.ToPILImage(),
+                                           transforms.Pad(2), transforms.ToTensor()])
+            else:
+                trsf = transforms.Compose([transforms.ToTensor()])
+            train_set = datasets.MNIST(root=f"Datasets/mnist/", train=False, download=False, transform=trsf)
+        case "cifar10":
+            mean = torch.tensor([0.4914, 0.4822, 0.4465])
+            std = torch.tensor([0.2023, 0.1994, 0.2010])
+
+            train_set = datasets.CIFAR10(root=f"Datasets/cifar10/", train=True,
+                                         download=False,
+                                         transform=transforms.Compose(
+                                             [transforms.ToTensor(),
+                                              transforms.Normalize(mean, std)]))
+        case "cifar100":
+            mean = torch.tensor([0.5071, 0.4867, 0.4408])
+            std = torch.tensor([0.2675, 0.2565, 0.2761])
+
+            train_set = datasets.CIFAR100(root=f"Datasets/cifar100/", train=True,
+                                         download=False,
+                                         transform=transforms.Compose(
+                                             [transforms.ToTensor(),
+                                              transforms.Normalize(mean, std)]))
+    return train_set
+
+algorithm = "RPSnet"
+dataset = "mnist"
+
+'''
+algorithm = sys.argv[1]
+dataset = sys.argv[2]
+#'''
+
+first_last_only = True
+if first_last_only:
+    filepath = f"analysis/{algorithm}/{dataset}/shap_values_first_last_1000_fixed"
+else:
+    filepath = f"analysis/{algorithm}/{dataset}/shap_values_full_1000"
+
+if not os.path.isdir(f"analysis/{algorithm}/{dataset}"):
+    os.makedirs(f"analysis/{algorithm}/{dataset}")
+
+
+setup_args(algorithm,dataset)
+print(f"Alg: {algorithm}\nDataset: {dataset}\nFirst/Last: {first_last_only}")
+num_tasks = 10 if dataset == "cifar100" else 5
+cls_per_task = 10 if dataset == "cifar100" else 2
+models = [load_model(algorithm, dataset, i, args=SalGenArgs.args) for i in range(num_tasks)]
+
+# Configures models depending on the algorithm chosen
+if algorithm == "iTAML":
+    for model in models: model.set_saliency(True)
+elif algorithm == "RPSnet":
+    infer_paths = []
+    lasts = [-1] * len(models)
+    for i in range(len(models)):
+        infer_paths.append(generate_path(i, SalGenArgs.dataset, SalGenArgs.args))
+        models[i].shap_path = infer_paths[i]
+        models[i].set_shap(True)
+elif algorithm == "der" or algorithm == "foster" or algorithm == "memo":
+    for model in models: model.set_shap(True)
+
+# Get train dataset
+train_set = get_train_set(dataset)
+
+# Get test dataset
+sal_dataloader = sdl.SalDataloader(SalGenArgs)
+for i in range(num_tasks):
+    if dataset == "cifar100":
+        sal_imgs, sal_labels, _, STD, MEAN = sal_dataloader.load_data(range(i * 10, (i * 10) + 10), 100, batch_size=10000)
+    else:
+        sal_imgs, sal_labels, _, STD, MEAN = sal_dataloader.load_data([i*2, (i*2)+1], 100, batch_size=10000)
+    print("Len of sal_imgs:", len(sal_imgs))
+    if i == 0:
+        test_imgs, test_labels = sal_imgs, sal_labels
+    else:
+        test_imgs = torch.cat((test_imgs, sal_imgs), 0)
+        test_labels = torch.cat((test_labels, sal_labels), 0)
+
+# Reshape MNIST test images for RPSnet
+if algorithm == "RPSnet" and dataset == "mnist":
+    test_imgs = test_imgs.detach().numpy().reshape(-1, 784)
+    test_imgs = torch.from_numpy(test_imgs)
+
+# Get background data for shap explainer
+if algorithm == "RPSnet" and dataset == "mnist":
+    background_data = torch.cat([x[0].unsqueeze(0).reshape(-1, 784) for x in train_set])  # Getting a sample
+else:
+    background_data = torch.cat([x[0].unsqueeze(0) for x in train_set])  # Getting a sample
+background_data = shap.sample(background_data, 100) # Taking 100 random samples
+
+#Create the explainer for each model
+explainers = [shap.GradientExplainer(models[i], background_data) for i in range(len(models))]
+
+shap_dict = {}
+all_shap_values = []
+all_indexes = []
+sample = 0
+
+for sample in tqdm(range(len(test_imgs)), desc="Progress"):
+    shap_dict[f'{sample}'] = {}
+    for e in range(len(explainers)):
+        if first_last_only:
+            boolean_statement = (e == test_labels[sample] // cls_per_task or e == num_tasks-1)
+        else:
+            boolean_statement = test_labels[sample] // cls_per_task <= e
+
+        if boolean_statement:
+            #print("Shape:", test_imgs.shape)
+            #print("Sample Shape:", test_imgs[sample].unsqueeze(0).shape)
+            shap_values, indexes = explainers[e].shap_values(test_imgs[sample].unsqueeze(0), ranked_outputs=1)
+            shap_dict[f'{sample}'][f'ses{e}'] = {'shap_values': shap_values, 'idxs': indexes}
+            shap_dict[f'{sample}']['true_label'] = test_labels[sample].item()
+        else:
+            continue
+
+# Save shap values to filepath
+np.save(filepath, shap_dict)
