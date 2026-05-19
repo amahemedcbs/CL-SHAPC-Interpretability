3add7544e0923ac995c1ba68530d15da64937ee6
AquaMouse247 <aquabanyie@gmail.com>
2026-05-19 18:12:55 -0400

    Stop tracking old project files.

--- old/run_jsd.ps1
+++
@@ -1,7 +0,0 @@
-# Using the 'py' launcher is recommended on Windows
-# as it will find the installed Python version automatically.
-.venv\Scripts\activate  
-Start-Process powershell -ArgumentList "-NoExit -Command & {
-       # Your script here
-       python jsd.py $alg mnist
-   }" -WindowStyle Hidden
