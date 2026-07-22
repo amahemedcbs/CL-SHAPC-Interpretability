d5f494ca94f0b86fc5a5a0a9edec014c0b015c78
AquaMouse247 <139708092+AquaMouse247@users.noreply.github.com>
2026-07-22 17:46:33 -0400

    Uploaded fixed versions of several files to fix mismatched model size bug.
    Uploaded fixed versions of utils/datasets.py, utils/load_models.py, and generate_shap_values.py. Review where changes were made to ensure no MedMNIST changes are overwritten.

---
+++ datasets_fixed.py
@@ -0,0 +1,43 @@
+# Imports
+from utils.model_parameters import iTAMLArgs
+
+
+def get_dataset_params(dataset):
+    if dataset == "cifar10":
+        return SHAPCifar10
+    if dataset == "cifar100":
+        return SHAPCifar100
+    if dataset == "tinyimagenet":
+        return SHAPTinyImageNet
+
+
+class SHAPDataset:
+    class_per_task = 2
+    num_class = 10
+    num_task = 5
+    shap_samples = 100
+
+    ###---Change made to allow for non-uniform task class sizes---###
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        if "init_cls" not in cls.__dict__:
+            cls.init_cls = cls.class_per_task
+    ###-----------------------------------------------------------###
+
+class SHAPCifar10(SHAPDataset):
+    class_per_task = 2
+    num_class = 10
+    num_task = 5
+    shap_samples = 100
+
+class SHAPCifar100(SHAPDataset):
+    class_per_task = 10
+    num_class = 100
+    num_task = 10
+    shap_samples = 20
+
+class SHAPTinyImageNet(SHAPDataset):
+    class_per_task = 20
+    num_class = 200
+    num_task = 10
+    shap_samples = 20
