1c6683427c2305a4b802ee4ba2726a42ea1e3e2e
Anwaar Majdi Ahemed <143734684+amahemedcbs@users.noreply.github.com>
2026-08-27 02:15:08 -0400

    Refactor SHAP visualization for new dataset and save path
    Updated algorithm and dataset for SHAP visualization. Added functionality to save plots to Google Drive.

--- visualize_shap.py
+++ visualize_shap.py
@@ -2,15 +2,16 @@
 import numpy as np
 import torch
 import shap
+import matplotlib.pyplot as plt
 
 # Custom Imports
-from utils.setup_args import SHAPArgs, create_shap_value_filepath, create_preds_savepath
+from setup_args_fixed import SHAPArgs, create_shap_value_filepath, create_preds_savepath
 import utils.shap_dataloader as sdl
 
 
 # Select Algorithm and Dataset
-algorithm = "memo"
-dataset = "cifar10"
+algorithm = "der"
+dataset = "dermamnist"
 shapArgs = SHAPArgs(algorithm, dataset)
 
 
@@ -21,86 +22,77 @@ num_tasks = shapArgs.dataset_params.num_task
 num_class = shapArgs.dataset_params.num_class
 cls_per_task = shapArgs.dataset_params.class_per_task
 shap_samples = shapArgs.dataset_params.shap_samples
+init_cls = getattr(shapArgs.dataset_params, 'init_cls', cls_per_task)
 
 
-shap_values_loaded = np.load(filepath, allow_pickle=True)  # ['shap_dict']
-#shap_values_loaded = np.load(f"analysis/{algorithm}/{dataset}/shap_values_first_last_1000.npy", allow_pickle=True)  # ['shap_dict']
+shap_values_loaded = np.load(filepath, allow_pickle=True)
 num_imgs = len(shap_values_loaded[()].keys())
 shap_dict = {}
 for i in range(num_imgs):
     shap_dict[f'{i}'] = shap_values_loaded[()][f'{i}']
 
-'''
-Bad Sample: 80
-Good Sample: 132
-
-Samples to Try: 55
-'''
-
-sample = 303
+sample = 30
 test_sample = shap_dict[f'{sample}']
 test_sess = list(test_sample.keys())
-test_sess.remove(test_sess[1])
-print(test_sess)
-ses, last_ses = int(test_sess[0][-1]), int(test_sess[1][-1])
-
-'''
-if dataset == "mnist":
-    test_shaps = [shap_dict[f'{sample}'][f'ses{ses}']['shap_values'].reshape(28,28,1),
-                  shap_dict[f'{sample}']['ses4']['shap_values'].reshape(28,28,1)]
-else:
-    test_shaps = [shap_dict[f'{sample}'][f'ses{ses}']['shap_values'].squeeze().reshape(32,32,3),
-                  shap_dict[f'{sample}']['ses4']['shap_values'].squeeze().reshape(32,32,3)]
-    print(test_shaps[0].shape)
-'''
+test_sess = [s for s in test_sess if s.startswith('ses')]
+ses, last_ses = int(test_sess[0].replace('ses', '')), int(test_sess[-1].replace('ses', ''))
 
 # Reshape/Transpose to Batch x Height x Width x Channel
 if dataset == "mnist":
-    test_shaps = [shap_dict[f'{sample}'][f'ses{ses}']['shap_values'].reshape(28,28,1),
-                  shap_dict[f'{sample}'][f'ses{last_ses}']['shap_values'].reshape(28,28,1)]
+    test_shaps = [shap_dict[f'{sample}'][f'ses{ses}']['shap_values'].reshape(28, 28, 1),
+                  shap_dict[f'{sample}'][f'ses{last_ses}']['shap_values'].reshape(28, 28, 1)]
 else:
-    test_shaps = [shap_dict[f'{sample}'][f'ses{ses}']['shap_values'].squeeze(-1).transpose([0, 2, 3, 1]),
-                  shap_dict[f'{sample}'][f'ses{last_ses}']['shap_values'].squeeze(-1).transpose([0, 2, 3, 1])]
+    s_first = shap_dict[f'{sample}'][f'ses{ses}']['shap_values']
+    s_last = shap_dict[f'{sample}'][f'ses{last_ses}']['shap_values']
+    if s_first.ndim == 5:
+        s_first = s_first.squeeze(-1)
+    if s_last.ndim == 5:
+        s_last = s_last.squeeze(-1)
+    if s_first.ndim == 4 and s_first.shape[1] in [1, 3]:
+        s_first = s_first.transpose([0, 2, 3, 1])
+    if s_last.ndim == 4 and s_last.shape[1] in [1, 3]:
+        s_last = s_last.transpose([0, 2, 3, 1])
+        
+    test_shaps = [s_first, s_last]
 
 # Get test dataset
 sal_dataloader = sdl.ShapDataloader(shapArgs)
 
 if dataset == "cifar100":
-    sal_imgs, sal_labels, _, STD, MEAN = sal_dataloader.load_data(range(ses * 10, (ses * 10) + 10), 20, batch_size=10000)
+    desired_cls = list(range(ses * 10, (ses * 10) + 10))
+    sal_imgs, sal_labels, _, STD, MEAN = sal_dataloader.load_data(desired_cls, 20, batch_size=10000)
 elif dataset == "imagenet200":
-    sal_imgs, sal_labels, _, STD, MEAN = sal_dataloader.load_data(range(ses * 20, (ses * 20) + 20), 20, batch_size=10000)
+    desired_cls = list(range(ses * 20, (ses * 20) + 20))
+    sal_imgs, sal_labels, _, STD, MEAN = sal_dataloader.load_data(desired_cls, 20, batch_size=10000)
 else:
-    ###---Updated to take initial classes learned into account---###
     if ses == 0:
-        sal_imgs, sal_labels, _, STD, MEAN = sal_dataloader.load_data(range(shapArgs.dataset_params.init_cls),
-                                                                      shapArgs.dataset_params.shap_samples, batch_size=10000)
+        desired_cls = list(range(init_cls))
     else:
-        sal_imgs, sal_labels, _, STD, MEAN = sal_dataloader.load_data(range((shapArgs.dataset_params.init_cls-cls_per_task)+ses*cls_per_task,
-                                                                        shapArgs.dataset_params.init_cls+(ses*cls_per_task)),
-                                                                        shapArgs.dataset_params.shap_samples, batch_size=10000)
-    ###----------------------------------------------------------###
+        start_c = init_cls + (ses - 1) * cls_per_task
+        end_c = init_cls + ses * cls_per_task
+        desired_cls = list(range(start_c, end_c))
+
+    sal_imgs, sal_labels, _, STD, MEAN = sal_dataloader.load_data(desired_cls, shap_samples, batch_size=10000)
+
 print("Len of sal_imgs:", len(sal_imgs))
 
 test_imgs, test_labels = sal_imgs, sal_labels
 
-# Get test image
-samples = range(shap_samples*(num_class-cls_per_task))
+# Compute local index within the loaded session images
+if ses == 0:
+    adj_sample = sample
+else:
+    adj_sample = sample - (init_cls * shap_samples + (ses - 1) * cls_per_task * shap_samples)
 
-adj_sample = sample - (ses * cls_per_task * shap_samples)
+adj_sample = min(adj_sample, len(test_imgs) - 1)
 
-test_img = test_imgs[adj_sample]#.unsqueeze(0)
+test_img = test_imgs[adj_sample]
 if dataset != "mnist":
     test_img = sal_dataloader.denormalize(test_img)
 test_img_np = np.transpose(test_img.numpy(), [1, 2, 0])
+test_img_np = np.clip(test_img_np, 0.0, 1.0)
 
-labels = [f'ses{ses}', f'ses{num_tasks-1}']
-#shap.image_plot(np.concatenate(test_shaps), np.stack([test_img_np,test_img_np]), true_labels=labels, cmap='plasma')
-
-
-# For a model with high SHAPC, but low accuracy:
-# Look for a sample that was predicted correctly, feature consistency should be high
-# Look for a sample that was predicted incorrectly, but check if feature consistency is still high -> indicates trustworthiness
-import matplotlib.pyplot as plt
+labels = [f'ses{ses}', f'ses{last_ses}']
 
 plt_fig, plt_axis = plt.subplots(2, 2, figsize=(6, 6))
 for ax in plt_axis[:, 0]:
@@ -112,7 +104,6 @@ for ax in plt_axis[:, 1]:
 s0 = np.sum(np.abs(test_shaps[0].squeeze()), axis=-1)
 s1 = np.sum(np.abs(test_shaps[1].squeeze()), axis=-1)
 
-# Normalize to [0, 1] across both or let imshow scale automatically
 vmax = max(s0.max(), s1.max())
 vmin = min(s0.min(), s1.min())
 
@@ -131,4 +122,16 @@ cbar.ax.set_title("Most Important", pad=8)
 cbar.ax.set_xlabel("Least Important", labelpad=8)
 
 plt.subplots_adjust(right=0.82)
+import os
+
+# Define your Google Drive save directory
+save_dir = f"/content/drive/MyDrive/CL-SHAPC-Interpretability/shap_visuals/{algorithm}/{dataset}"
+os.makedirs(save_dir, exist_ok=True)
+
+save_path = os.path.join(save_dir, f"shap_sample_{sample}.png")
+
+# Save high-resolution figure before showing
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
+print(f"Saved plot to: {save_path}")
+
 plt.show()
